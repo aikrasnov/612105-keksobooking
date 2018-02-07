217c19e9b36a5d527da231472723624806b0d458
akrasnov <akrasnov96@yandex-team.ru>
2018-02-07 09:49:17 +0300

    Исправления по замечаниям

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const args = process.argv.slice(2);
-const version = '0.0.1';
+const ARGS = process.argv.slice(2);
+const VERSION = `0.0.1`;
 
-switch (args[0]) {
-  case '--help':
-  case '-help':
-    console.log('Доступные команды:\n' +
-      '--help    — печатает этот текст;\n' +
-      '--version — печатает версию приложения;');
+switch (ARGS[0]) {
+  case `--help`:
+  case `-help`:
+    console.log(`Доступные команды:\n--help    — печатает этот текст;\n--version — печатает версию приложения;`);
     process.exit(0);
+    break;
 
-  case '--version':
-  case '-version':
-    console.log(`Версия ${version}`);
+  case `--version`:
+  case `-version`:
+    console.log(`Версия ${VERSION}`);
     process.exit(0);
+    break;
 
   case undefined:
-    console.log('Привет пользователь!\n' +
-      'Эта программа будет запускать сервер «Кексобукинг».\n' +
-      'Автор: Александр Краснов.');
+    console.log(`Привет пользователь!\nЭта программа будет запускать сервер «Кексобукинг».\nАвтор: Александр Краснов.`);
     process.exit(0);
+    break;
 
   default:
-    console.error(`Неизвестная команда ${args[0]}. Чтобы прочитать правила использования приложения, наберите "--help"`);
+    console.error(`Неизвестная команда ${ARGS[0]}. Чтобы прочитать правила использования приложения, наберите "--help"`);
     process.exit(1);
 
 }
